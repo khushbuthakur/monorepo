4c8fa23e82ad7a7662c728d2a270b0ea537b4abc
khushbuthakur <thakurkhushbu14@gmail.com>
2020-09-17 15:42:23 +0530

    Updates

---
+++ precache-manifest.22df55e3da28e0527554208e214a88a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf3d5f98fe721537f2d288aad158eb01",
+    "url": "/khushbuthakur/monorepo/home/index.html"
+  },
+  {
+    "revision": "9a6fd707636162123b94",
+    "url": "/khushbuthakur/monorepo/home/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9ca599565b6bfb4a542b",
+    "url": "/khushbuthakur/monorepo/home/static/js/2.72e4222a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/khushbuthakur/monorepo/home/static/js/2.72e4222a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a6fd707636162123b94",
+    "url": "/khushbuthakur/monorepo/home/static/js/main.44b92e1d.chunk.js"
+  },
+  {
+    "revision": "55529cfa09d552b4b507",
+    "url": "/khushbuthakur/monorepo/home/static/js/runtime-main.20927f82.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/khushbuthakur/monorepo/home/static/media/logo.5d5d9eef.svg"
+  }
+]);
